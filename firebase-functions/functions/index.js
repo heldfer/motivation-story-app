215d7c053463ccd96a600b77988e2f365a38af60
Hélder Ferreira <helderemanuelferreira@gmail.com>
2020-04-10 18:59:30 +0100

    Configured the project to run firebase-functions locally

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,21 +1,26 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions').region('europe-west2');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://motivation-story-app.firebaseio.com"
+});
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions
-  .region('europe-west2')
   .https.onRequest((req, res) => {
     res.send("Hello Helder from europe location!");
   });
 
 exports.getStories = functions
-  .region('europe-west2')
   .https.onRequest((req, res) => {
-    admin.firestore().collection('stories').get()
+    admin.firestore()
+      .collection('stories')
+      .get()
       .then(data => {
         let stories = [];
         data.forEach(doc => {
@@ -24,6 +29,27 @@ exports.getStories = functions
         return res.json({ data: stories });
       })
       .catch(err => {
-        console.log('Error getting documents ', err)
+        console.log('Error getting stories ', err)
+      })
+  });
+
+exports.createStory = functions
+  .https.onRequest((req, res) => {
+    const newStory = {
+      user: req.body.user,
+      description: req.body.description,
+      title: req.body.title,
+      createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+      .collection('stories')
+      .add(newStory)
+      .then(storyCreated => {
+        return res.json({ message: `Story with id ${storyCreated.id} created successfully!`, data: storyCreated })
+      })
+      .catch(error => { 
+        res.status(500).json({ error, message: 'Ups! Something went wrong.' })
+        console.error(error)
       })
   });
